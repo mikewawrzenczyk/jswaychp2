d7c2da75dd21ebf69f884af6c33cacdbce9dfac2
Mike Wawrzenczyk <mike.wawrzenczyk@gmail.com>
2022-03-13 16:12:22 -0400

    Updated JS file

---
+++ main.js
@@ -0,0 +1,34 @@
+// let firstName = prompt('Enter First Name')
+// let lastName = prompt('Enter Last Name')
+
+// console.log(firstName + ' ' + lastName)
+
+// let a = 2;
+// a -= 1;
+// a++;
+// let b = 8;
+// b += 2;
+// const c = a + b * b;
+// const d = a * b + b;
+// const e = a * (b + b);
+// const f = a * b / a;
+// const g = b / a * a;
+// console.log(a, b, c, d, e, f, g);
+
+// let vat = Number(prompt('Enter a Vat Number'))
+// console.log(vat +  vat * 0.26)
+
+//The conversion between scales is given by the formula: [°F] = [°C] x 9/5 + 32.
+// let celcius = Number(prompt('Enter Celcius to be converted to Farhenheit'))
+// console.log(celcius * 9/5 + 32)
+
+let number1 = 5;
+let number2 = 3;
+let numberTemp = number2
+number2= number1
+number1= numberTemp
+// TODO: type your code here (and nowhere else!)
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
+//#gitchangetest
